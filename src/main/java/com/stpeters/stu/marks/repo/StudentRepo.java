162e840c27e981434c00a042510655aa7d121db5
sharifabee <sharifabee.alur@gmail.com>
2025-02-19 15:10:42 +0530

    Student rec

---
+++ src/main/java/com/stpeters/stu/marks/repo/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.stpeters.stu.marks.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.stpeters.stu.marks.entity.Student;
+
+public interface StudentRepo extends JpaRepository<Student, Integer>{
+
+}
