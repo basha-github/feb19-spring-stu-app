a4bd824783e19058925bb8ed2ab4fb1ee3effc1a
sharifabee <sharifabee.alur@gmail.com>
2025-02-19 14:55:37 +0530

    first commit

---
+++ src/test/java/com/stpeters/stu/marks/StuMngAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpeters.stu.marks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuMngAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
