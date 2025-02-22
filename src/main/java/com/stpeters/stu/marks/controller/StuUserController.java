ad3a646b5e3bade0286100e980a84853c0042515
sharifabee <sharifabee.alur@gmail.com>
2025-02-22 11:31:52 +0530

    rud operations

--- src/main/java/com/stpeters/stu/marks/controller/StuUserController.java
+++ src/main/java/com/stpeters/stu/marks/controller/StuUserController.java
@@ -1,9 +1,15 @@
 package com.stpeters.stu.marks.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stpeters.stu.marks.entity.StuUsers;
@@ -43,6 +49,33 @@ public class StuUserController {
 		
 		return  serVar.createNewUser(data);
 	}
+	
+	@PutMapping("/stu/mng/users/update")
+	public String updateUser(@RequestBody StuUsers data) {
+		
+		
+		return  serVar.updateUser(data);
+	}
+	@DeleteMapping("/stu/mng/users/delete")
+	public String deleteUser(@RequestParam String email) {
+		
+		
+		return  serVar.deleteUser(email);
+	}
+	@GetMapping("/stu/mng/users/get/all")
+	public List<StuUsers> getAllUsers() {
+		
+		
+		return  serVar.getAllUsers();
+	}
+	
+	@GetMapping("/stu/mng/search/user")
+	public boolean searchUser(@RequestParam String email) {
+		
+		
+		return  serVar.searchUser(email);
+	}
+	
 	
 
 }
